bd77fe02ac4bb598c683ce68795bd07dbdc383f8
Vitaly <adancedwin@gmail.com>
2018-07-29 22:25:51 +0300

    main class for using Fraction class as a client

--- src/FractionCalculator.java
+++ src/FractionCalculator.java
@@ -8,16 +8,37 @@ public class FractionCalculator {
     }
 
     public static char getOperation(){
-        System.out.println("Enter in a valid mathematical operation (\"+\", \"-\n" + "\", \"/\", \"*\", \"=\", \"q\", or \"Q\"): ");
+        System.out.println("Enter in a valid mathematical operation (\"+\", \"-\n \", \"/\", \"*\", \"=\", \"q\", or \"Q\" to quit): ");
         Scanner input = new Scanner(System.in);
         String signStr = input.next();
         char signCh = signStr.charAt(0);
-        while(!(evaluationOfSign(signCh))){
+        while(invalidSign(signCh)&&(signStr.length()>1)){
+            System.out.println("Invalid input (\"+\", \"-\n \", \"/\", \"*\", \"=\", \"q\", or \"Q\" to quit): ");
+            signStr = input.next();
+            signCh = signStr.charAt(0);
+        }
+        return signCh;
+    }
 
+    public static boolean invalidSign(char sign){ //evaluate - is the input invalid
+        if(sign=='+'){
+            return false;
+        }else if(sign=='-'){
+            return false;
+        }else if(sign=='/'){
+            return false;
+        }else if(sign=='*'){
+            return false;
+        }else if(sign=='='){
+            return false;
+        }else{
+            return true;
         }
     }
 
-    public static boolean evaluationOfSign(char sign){
-
+    public static void validFraction(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a fraction (a/b) or integer: ");
+        
     }
 }
