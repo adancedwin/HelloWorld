f439335bcafaf485a9d77f44064fda405ac0afb2
Vitaly <adancedwin@gmail.com>
2018-07-29 17:56:30 +0300

    class Fraction is ready for use

--- src/Fraction.java
+++ src/Fraction.java
@@ -5,9 +5,11 @@ public class Fraction {
     public Fraction(int usrNumerator, int usrDenominator){
         if(usrDenominator==0){
             throw new IllegalArgumentException("The denominator must not be zero!");
+            //checking if values are negative, if so - convert them to absolute values
         }else if((usrDenominator<0)&&(usrNumerator<0)){
             usrDenominator = Math.abs(usrDenominator);
             usrNumerator = Math.abs(usrNumerator);
+            //in case denominator has a negative value - bump this trait to a numerator
         }else if((usrDenominator<0)&&(usrNumerator>0)){
             usrDenominator = Math.abs(usrDenominator);
             usrNumerator*=-1;
@@ -24,8 +26,66 @@ public class Fraction {
         this(usrNumerator, 1);
     }
 
+    public int getNumerator() {
+        return numerator;
+    }
 
+    public int getDenominator() {
+        return denominator;
+    }
 
+    public String toString(){ //convert the equation to a String for printing out on client
+        return numerator + "/" + denominator;
+    }
+
+    public double toDouble(){ //get the result of an equation in double type
+        return numerator / denominator;
+    }
+
+    //add the results of two equations to one another and return the result within a new object
+    public Fraction add(Fraction other){
+        int toNewFraction = (int)((this.toDouble())+(other.toDouble()));
+        Fraction otherFraction = new Fraction(toNewFraction);
+        return otherFraction;
+    }
+
+    public Fraction subtract(Fraction other){
+        int toNewFraction = (int)((this.toDouble())-(other.toDouble()));
+        Fraction otherFraction = new Fraction(toNewFraction);
+        return otherFraction;
+    }
+
+    public Fraction multiply(Fraction other){
+        int toNewFraction = (int)((this.toDouble())*(other.toDouble()));
+        Fraction otherFraction = new Fraction(toNewFraction);
+        return otherFraction;
+    }
+
+    public Fraction divide(Fraction other){
+        int toNewFraction = (int)((this.toDouble())/(other.toDouble()));
+        Fraction otherFraction = new Fraction(toNewFraction);
+        return otherFraction;
+    }
+
+    public boolean equals(Fraction other){
+        return (this.toDouble()==(other.toDouble()));
+    }
+
+    public void toLowestTerms(){
+        int gdc = Fraction.gcd(this.numerator, this.denominator);
+        numerator/=gdc;
+        denominator/=gdc;
+    }
+
+    public static int gcd(int num, int den){
+        int aReminder=0;
+        while((num!=0)&&(den!=0)){
+            aReminder = num/den;
+            num = den;
+            den = aReminder;
+        }
+        return aReminder;
+    }
 }
 
 
