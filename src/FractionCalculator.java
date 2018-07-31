ac574136aecbc455deea011cbfce4d861a5b184d
Vitaly <adancedwin@gmail.com>
2018-07-31 22:08:22 +0300

    some work is made on the "validFraction" method, yet not fully implemented

--- src/FractionCalculator.java
+++ src/FractionCalculator.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class FractionCalculator {
     public static void main(String[] args){
         getOperation();
-        validFraction();
-        getFraction();
+        String fractionStr = validFraction(false);
+        //getFraction();
     }
 
     public static char getOperation(){
@@ -36,9 +36,46 @@ public class FractionCalculator {
         }
     }
 
-    public static void validFraction(){
+    public static String validFraction(boolean nextTry){
         Scanner input = new Scanner(System.in);
-        System.out.println("Please enter a fraction (a/b) or integer: ");
-        
+        if(nextTry){//is it the first attempt to input a fraction of an integer?
+            System.out.println("Incorrect input! Please enter a fraction (a/b) or an integer: ");
+        }else{
+            System.out.println("Please enter a fraction (a/b) or an integer: ");
+        }
+        String fraction = input.next();
+        //determine whether the input is a fraction or an integer
+        if((fraction.indexOf('/')==-1)){ //true means there is an integer
+            String fractionInt = validFractionHalf(fraction);
+        }
+        String fractionStr = validFractionFull(fraction); //there is a fraction, presumably
+
     }
+
+    public static String validFractionHalf(String half){
+
+    }
+
+    public static String validFractionFull(String full){
+        int iCo=0;
+        Character charBuff;
+        String validFractionStr="";
+        if(full.charAt(0)=='-'){
+            iCo=1; //a fraction starts with '-', skip first character (we need that, it's valid)
+            validFractionStr+='-';
+        }else if(full.charAt(0)!='-'){
+            iCo = 0; //a fraction doesn't start with '-'
+        }
+        for(int i=iCo;i<=full.length();i++){
+            charBuff = full.charAt(i);
+            if((Character.isDigit(charBuff))||(full.charAt(i)=='/')){
+                validFractionStr+=charBuff;
+            }else{
+                validFraction(true); //input is incorrect, try over again
+            }
+        }
+        return validFractionStr;
+    }
+
+
 }
